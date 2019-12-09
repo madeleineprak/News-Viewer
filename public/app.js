f351f95c483c64967c9ed1ab74301532ea058a20
Madeleine Prak <madeleineprak@gmail.com>
2019-12-08 17:47:55 -0800

    Fix delete note button and card styling

--- public/app.js
+++ public/app.js
@@ -1,14 +1,4 @@
-// Grab the articles
-// $.getJSON("/articles", function(data) {
-//     for (var i = 0; i < data.length; i++) {
-//       // Display articles on page
-//     //   $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + "<a href='" + data[i].link + "'>Read More</a>" + "</p><a class='button is-primary'>Save</a>");
-//     $("#articles").append("<div class='card'><div class='card-content'><p class='title' data-id='" + data[i]._id + "'>" + data[i].title + "</p><a class='button is-primary save-article-button'data-id='" + data[i]._id + "'>Save</a></div></div></div>");
-//     // $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "</p><a class='button is-primary'>Save</a>");
-//     }
-// });
 $(document).on("click", ".navbar-burger", function() {
-    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
 })
@@ -53,7 +43,7 @@ $(document).on("click", ".notes-button", function() {
         $("#notes").append("<input id='titleinput' name='title' class='input' type='text'>");
         $("#notes").append("<textarea class='textarea' id='bodyinput' name='body'></textarea>");
         $("#notes").append("<a class='button is-light savenote' data-id='" + data._id + "'>Save Note</a>");
-        $("#notes").append("<a class='button is-danger deletenote' data-id='" + data._id + "'>Delete Note</a>");
+        $("#notes").append("<a class='button is-danger deletenote' data-id='" + data.note._id + "'>Delete Note</a>");
   
         // If there's a note in the article
         if (data.note) {
